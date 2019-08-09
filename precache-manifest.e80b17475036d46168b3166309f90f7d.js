65ea668040e2bf4dd9dd12e4baf433bc84375a68
Grace0 <gracekwak25@gmail.com>
2019-08-09 10:29:19 -0700

    Updates

---
+++ precache-manifest.e80b17475036d46168b3166309f90f7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572c72e06caed8aacb0de3e7d50f81d4",
+    "url": "/ideo/index.html"
+  },
+  {
+    "revision": "f399fb2953e86ad81b18",
+    "url": "/ideo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7684408bd5554961719d",
+    "url": "/ideo/static/js/2.4f057907.chunk.js"
+  },
+  {
+    "revision": "f399fb2953e86ad81b18",
+    "url": "/ideo/static/js/main.1af4b1c8.chunk.js"
+  },
+  {
+    "revision": "d99450ff06842870a4de",
+    "url": "/ideo/static/js/runtime~main.c9d4cfcc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ideo/static/media/logo.5d5d9eef.svg"
+  }
+]);
